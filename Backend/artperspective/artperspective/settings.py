68930cd411e53175ca39ad060bc597292262a701
alexunder2003 <nazimov.amu@yandex.ru>
2025-05-20 15:53:11 +0300

    Alter settings + env

--- Backend/artperspective/artperspective/settings.py
+++ Backend/artperspective/artperspective/settings.py
@@ -25,9 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-a+58hbby455^o)j_$-8(5@&c2%4=!2x2o-!y(#siu5n3d3j+8k"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
